7e5e9a64a52d84a86a97fcc1a1c376de0de0f9cd
unknown <alelid95gmail.com>
2024-04-02 19:55:22 +0200

    created sign uo component, the form input component, button component

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,77 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCAypf3hHdNG5lFfSiC7gwyUehI4ykam7Q",
+
+  authDomain: "crwn-clothing-db-b70d0.firebaseapp.com",
+
+  projectId: "crwn-clothing-db-b70d0",
+
+  storageBucket: "crwn-clothing-db-b70d0.appspot.com",
+
+  messagingSenderId: "842115802427",
+
+  appId: "1:842115802427:web:d8e67cdb6dbec12d3c831e"
+
+};
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
